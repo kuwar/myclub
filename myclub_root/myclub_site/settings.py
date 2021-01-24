7ed3d7bdac9d2f077de8d30df1cdd8351b52e936
kuwar <kuwar@tradeinsur.com>
2021-01-24 12:52:40 +0100

    environment managed for db

--- myclub_root/myclub_site/settings.py
+++ myclub_root/myclub_site/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -26,7 +26,6 @@ SECRET_KEY = '@jp&+pf=((i0v^r2^a0s&k@_b$utbopzkx1vori9u508v)uk)j'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myclub_site.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -84,14 +82,13 @@ WSGI_APPLICATION = 'myclub_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myclub',
-        'USER': 'myclub',
-        'PASSWORD': 'myclub21',
-        'HOST': 'myclub.cycltcghndfr.eu-west-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'NAME': os.environ.get('POSTGRESQL_DB_NAME', 'defaul_value'),
+        'USER': os.environ.get('POSTGRESQL_DB_USER', 'defaul_value'),
+        'PASSWORD': os.environ.get('POSTGRESQL_DB_PASSWORD', 'defaul_value'),
+        'HOST': os.environ.get('POSTGRESQL_DB_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRESQL_DB_USER', '5432'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -111,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -124,7 +120,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
