178a49d8243a3b0cfd68c57a70e9ee3d243f871b
kuwar <kuwar@tradeinsur.com>
2021-02-07 15:36:13 +0100

    admin event formset added and activated password reset

--- myclub_root/events/admin.py
+++ myclub_root/events/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Venue, MyClubUser, Event
+
+
+# admin.site.register(Venue)
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone')
+    ordering = ('name',)
+    search_fields = ('name', 'address')
+
+
+class MyClubUserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    ordering = ('first_name',)
+    search_fields = ('first_name', 'last_name', 'email')
+
+
+admin.site.register(MyClubUser, MyClubUserAdmin)
+
+
+# admin.site.register(Event)
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    # fields = (('name', 'venue'), 'event_date', 'description', 'manager')
+    list_display = ('name', 'event_date', 'venue')
+    list_filter = ('event_date', 'venue')
+    ordering = ('-event_date',)
+    fieldsets = (
+        ('Required Information', {
+            'description': "These fields are required for each event.",
+            'fields': (('name', 'venue'), 'event_date')
+        }),
+        ('Optional Information', {
+
+            'classes': ('collapse',),
+
+            'fields': ('description', 'manager')
+        }),
+    )
--- myclub_root/events/models.py
+++ myclub_root/events/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Venue(models.Model):
     name = models.CharField('Venue Name', max_length=120)
     address = models.CharField(max_length=300)
     zip_code = models.CharField('Zip/Post Code', max_length=12)
-    phone = models.CharField('Contact Phone', max_length=20)
-    web = models.URLField('Web Address')
-    email_address = models.EmailField('Email Address')
+    phone = models.CharField('Contact Phone', max_length=20, blank=True)
+    web = models.URLField('Web Address', blank=True)
+    email_address = models.EmailField('Email Address', blank=True)
 
     def __str__(self):
         return self.name
@@ -26,9 +27,11 @@ class Event(models.Model):
     name = models.CharField('Event Name', max_length=120)
     event_date = models.DateTimeField('Event Date')
     venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
-    manager = models.CharField(max_length=60)
+    # manager = models.CharField(max_length=60)
+    manager = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     # give related_name to retrive from myclubuser
-    attendees = models.ManyToManyField(MyClubUser, blank=True, related_name='user_event')
+    attendees = models.ManyToManyField(
+        MyClubUser, blank=True, null=True, related_name='user_event')
     description = models.TextField(blank=True)
 
     def __str__(self):
